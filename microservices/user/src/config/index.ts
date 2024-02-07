6c07afba8f80d0fea28989c1c3c590b5a8c1dd7d
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-02-07 22:31:32 +0530

    port error, finally fixed 😭

--- microservices/user/src/config/index.ts
+++ microservices/user/src/config/index.ts
@@ -11,10 +11,8 @@ dotenv.config();
 /* ------- PORTS ------- */
 export const PORT = process.env.PORT;
 
-/* ------- DATABASE ------- */
+/* ------- SUPABASE ------- */
 export const DATABASE_URL = process.env.DATABASE_URL || "";
 
+/* ------- POSTGRES ------- */
 export const POSTGRES_URL = process.env.POSTGRES_URL || "";
-export const POSTGRES_USER = process.env.POSTGRES_USER || "";
-export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || "";
-export const POSTGRES_NAME = process.env.POSTGRES_NAME || "";
