f3cf5a54fe8a6f11c2d238412d915baf447576d6
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-02-08 10:14:00 +0530

    Update db.ts

--- microservices/user/src/database/db.ts
+++ microservices/user/src/database/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import { DATABASE_URL, POSTGRES_URL } from "../config";
+import { SUPABASE_URL, POSTGRES_URL } from "../config";
 
 const pool = new Pool({
   connectionString: POSTGRES_URL,
